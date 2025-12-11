932f56557f6f35d2498884e083aba8371cf8703c
Wai Yee <wwaiyyee@gmail.com>
2025-12-12 01:16:43 +0800

    Update app/learnword/components/LearnWordNavbar.tsx - 2025-12-12 01:16:43

--- app/learnword/components/LearnWordNavbar.tsx
+++ app/learnword/components/LearnWordNavbar.tsx
@@ -3,9 +3,13 @@
 import React from "react";
 import Link from "next/link";
 
-export default function LearnWordNavbar() {
+interface LearnWordNavbarProps {
+    onHomeClick?: () => void;
+}
+
+export default function LearnWordNavbar({ onHomeClick }: LearnWordNavbarProps) {
     return (
-        <div className="absolute top-0 left-0 w-full p-6 flex justify-between items-start z-50 pointer-events-none">
+        <div className="absolute top-0 left-0 w-full px-6 pt-0 flex justify-between items-start z-50 pointer-events-none">
             {/* Left - Title */}
             <div className="pointer-events-auto">
                 <img
@@ -17,15 +21,16 @@ export default function LearnWordNavbar() {
 
             {/* Right - Home Button */}
             <div className="pointer-events-auto">
-                <Link href="/room">
-                    <div className="w-16 h-16 transition-transform hover:scale-110 active:scale-95 cursor-pointer">
-                        <img
-                            src="/Navbar/Home.png"
-                            alt="Home"
-                            className="w-full h-full object-contain drop-shadow-md"
-                        />
-                    </div>
-                </Link>
+                <div
+                    onClick={onHomeClick}
+                    className="w-16 h-16 transition-transform hover:scale-110 active:scale-95 cursor-pointer"
+                >
+                    <img
+                        src="/Navbar/Home.png"
+                        alt="Home"
+                        className="w-full h-full object-contain drop-shadow-md"
+                    />
+                </div>
             </div>
         </div>
     );
